b2865533da7f6f8b95f56d8594b02783440b4349
brown2206 <brown.angelo.jr@gmail.com>
2018-08-11 22:08:29 -0400

    change resource

--- app/admin/guests.rb
+++ app/admin/guests.rb
@@ -1,4 +1,5 @@
-ActiveAdmin.register Guest do
+ActiveAdmin.register Guest, as: "Dog" do
+    menu label: "Adoptable Dogs"
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
