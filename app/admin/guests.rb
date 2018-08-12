675c0f05ecec830950b152db636e29cf044e4b34
brown2206 <brown.angelo.jr@gmail.com>
2018-08-11 21:45:37 -0400

    edit guest

--- app/admin/guests.rb
+++ app/admin/guests.rb
@@ -2,20 +2,26 @@ ActiveAdmin.register Guest do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :list, :of, :attributes, :on, :model
+permit_params :name, :breed, :gender, :age, :size, :color, :shelter_name,
+:shelter_address, :shelter_city, :shelter_state, :shelter_zip, :phone_number, :url
 
 index do
   selectable_column
-  column "Test", :name
+  column "ID", :id
+  column "Name", :name
+  column "Breed", :breed
+  column "Gender", :gender
+  column "Age", :age
+  column "Size", :size
+  column "Color", :color
+  column "Shelter Name", :shelter_name
+  column "Street Address", :shelter_address
+  column "City", :shelter_city
+  column "State", :shelter_state
+  column "Zip Code", :shelter_zip
+  column "Phone Number", :phone_number
+  column "Website", :url
   actions
 end
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
 end
